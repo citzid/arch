869ce6d1dd8807ac4ddbf4fab3e996afafae1d45
citz <co.jingga@gmail.com>
2022-08-22 18:32:40 +0700

    Update zshohmy.sh
    fix clone plugin zsh git

--- bspwm/sh/zshohmy.sh
+++ bspwm/sh/zshohmy.sh
@@ -12,14 +12,16 @@ sudo pacman -S zsh
 
 ##Oh My Zsh Install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-chsh -s $(which zsh)
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-home/$username/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-home/$username/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-home/$username/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+cd /home/$username/.oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/zsh-users/zsh-history-substring-search
 
 cd home/$username/.oh-my-zsh
 git clone https://github.com/reobin/typewritten.git
 
 sudo cp -R /home/$username/arch/bspwm/home/zshrc /home/$username/.zshrc
+
+chsh -s $(which zsh)
 
 
