93ac44e7a619dc4eac35183eae86c08ac2d3c4a5
citz <co.jingga@gmail.com>
2022-08-22 11:22:26 +0700

    Update sddm.sh

--- bspwm/sh/sddm.sh
+++ bspwm/sh/sddm.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #RUN this after installing base Debian with no GUI and install git
 #Made by @citz_id
+username=$(id -u -n 1000)
 
 sudo pacman -S install -y sddm
 sudo systemctl enable sddm
+
+cp -R /home/$username/arch/bspwm/home/xinitrc /home/$username/.xinitrc
+cp -R /home/$username/arch/bspwm/home/zshrc /home/$username/.zshrc
+cp -R /home/$username/arch/bspwm/home/bashrc /home/$username/.bashrc
+cp -R /home/$username/arch/bspwm/config/* /home/$username/.config/
 
 chown $username:$username /home/$username/
 chown $username:$username /home/$username/.config/*
