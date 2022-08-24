cdc1736b99a5fd4987d1098f1b436f854f06d2e6
citz <co.jingga@gmail.com>
2022-08-24 12:35:09 +0700

    Update dependencies.sh
    update for bypass confirm Y/n...

--- bspwm/sh/dependencies.sh
+++ bspwm/sh/dependencies.sh
@@ -7,20 +7,20 @@ mkdir -p /home/$username/.config/{bspwm,sxhkd}
 
 ##################################################################################
 timedatectl set-ntp true
-sudo pacman -Syyu
+yes | sudo pacman -Syyu
 
 ## Display manager
-sudo pacman -S sddm
+yes | sudo pacman -S sddm
 sudo systemctl enable sddm
 
 
 
 ## DEPENDENCIES
-sudo pacman -S base-devel git --needed cargo meson intel-vulkan xf86-video-intel
-sudo pacman -S unzip unrar tar ranger conky htop neofetch
+yes | sudo pacman -S base-devel git --needed cargo meson intel-vulkan xf86-video-intel
+yes | sudo pacman -S unzip unrar tar ranger conky htop neofetch
 
 ## AUDIO
-sudo pacman -S pulseaudio pulseaudio-alsa alsa-utils pavucontrol
+yes | sudo pacman -S pulseaudio pulseaudio-alsa alsa-utils pavucontrol
 
 #mpd
 mkdir -p home/$username/.config/mpd
@@ -30,31 +30,31 @@ chmod +x /home/$username/.config/mpd/mpd.conf
 /etc/rc.d/mpd create-db
 /etc/rc.d/mpd start
 
-pacman -Sy ncmpc mpc gmpc glurp
+yes | sudo pacman -S ncmpc mpc gmpc glurp
 
 ## Installing Codecs and plugins
-sudo pacman -S libvorbis libxv wavpack x264 xvidcore
-sudo pacman -S a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora 
+yes | sudo pacman -S libvorbis libxv wavpack x264 xvidcore
+yes | sudo pacman -S a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora 
 
 ## Isntall Themes
-sudo pacman -S numix-themes
+yes | sudo pacman -S numix-themes
 
 # Install & enable firewall
-sudo pacman -S ufw && sudo ufw enable
+yes | sudo pacman -S ufw && sudo ufw enable
 
 # Install Microcode
 # INTEL
-sudo pacman -S intel-ucode
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+yes | sudo pacman -S intel-ucode
+yes | sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # AMD
-sudo pacman -S linux-firmware
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+yes | sudo pacman -S linux-firmware
+yes | sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 
 ## Activate user-level units
 #Start & enable both user-level services:
-sudo pacman -S pipewire pipewire-pulse 
+yes | sudo pacman -S pipewire pipewire-pulse 
 
 systemctl --user enable pipewire.service
 systemctl --user start pipewire.service
@@ -64,12 +64,12 @@ systemctl --user start pipewire-pulse.service
 
 
 ## Activate MTP uSB devices
-sudo pacman -S mtpfs -y
+yes | sudo pacman -S mtpfs
 cd home/$username/Downloads
 git clone https://aur.archlinux.org/jmtpfs.git
 cd jmtpfs
 makepkg -s
-sudo pacman -U jmtpfs-0.5-3-x86_64.pkg.tar.zst
-sudo pacman -Sy gvfs-mtp -y
-sudo pacman -Sy gvfs-gphoto2 -y
+yes | sudo pacman -U jmtpfs-0.5-3-x86_64.pkg.tar.zst
+yes | sudo pacman -Sy gvfs-mtp
+yes | sudo pacman -Sy gvfs-gphoto2
 
