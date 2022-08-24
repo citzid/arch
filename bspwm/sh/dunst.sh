af4361ef93d75c8a77ff917b7bb5850c18686e88
citz <co.jingga@gmail.com>
2022-08-24 12:36:13 +0700

    Update dunst.sh

--- bspwm/sh/dunst.sh
+++ bspwm/sh/dunst.sh
@@ -10,8 +10,8 @@ username=$(id -u -n 1000)
 git clone https://github.com/dunst-project/dunst.git
 cd dunst
 make -j5
-sudo make install
+yes | sudo make install
 
 sudo mkdir /home/$username/.config/dunst
 sudo cp /home/$username/arch/bspwm/dunst/dunstrc /home/$username/.config/dunst/dunstrc
-sudo chown $username:$username /home/$usernamer/.config/dunst/dunstrc
+sudo chown $username:$username /home/$username/.config/dunst/dunstrc
