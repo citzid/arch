7459c6f0f485898be0284040391a10280a4f0d2b
citz <co.jingga@gmail.com>
2022-08-24 12:40:09 +0700

    Update polybar.sh

--- bspwm/sh/polybar.sh
+++ bspwm/sh/polybar.sh
@@ -14,8 +14,8 @@ username=$(id -u -n 1000)
 #sudo apt-get -y install xcb-proto libxcb-xrm-dev i3-wm libasound2-dev
 #sudo apt-get -y install libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev
 
-sudo pacman -S -y build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
-sudo pacman -S -y libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+yes | sudo pacman -S build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+yes | sudo pacman -S libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
 
 
 #Clone the source for Polybar#
@@ -25,7 +25,7 @@ sudo pacman -S -y libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev
 #cd polybar && ./build.sh
 
 # Online install #
-sudo pacman -S -y polybar
+yes | sudo pacman -S polybar
 mkdir -p  /home/$username/.config/polybar
 cp /home/$username/arch/bspwm/config/polybar/* /home/$username/.config/polybar
 chmod +x /home/$username/.config/polybar/*
